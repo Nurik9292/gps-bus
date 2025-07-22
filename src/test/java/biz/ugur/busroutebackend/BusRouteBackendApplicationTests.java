34b8405e77f0e3777e996f10e4d53e6ac539d635
Timur <nurydavletov@gmail.com>
2025-07-22 15:41:12 +0300

    pom setup

---
+++ src/test/java/biz/ugur/busroutebackend/BusRouteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.ugur.busroutebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusRouteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
