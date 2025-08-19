bf46c8ca4a678da13cb686d5bef12bfb10a67aaa
Timur <nurydavletov@gmail.com>
2025-08-19 06:19:33 +0300

    new docker compose for prod

--- src/main/java/biz/ugur/busroutebackend/shared/infrastructure/config/DatabaseConfig.java
+++ src/main/java/biz/ugur/busroutebackend/shared/infrastructure/config/DatabaseConfig.java
@@ -47,11 +47,6 @@ public class DatabaseConfig extends AbstractR2dbcConfiguration {
     @Value("${spring.r2dbc.pool.max-size}")
     private Integer maxPoolSize;
 
-    @Value("${spring.r2dbc.pool.max-idle-time}")
-    private Integer maxIdleTime;
-
-    @Value("${spring.r2dbc.pool.max-create-connection-time}")
-    private Integer maxCreateConnectionTime;
 
 
     @Bean
@@ -65,14 +60,14 @@ public class DatabaseConfig extends AbstractR2dbcConfiguration {
                         .database(database)
                         .username(username)
                         .password(password)
-                        .connectTimeout(Duration.ofSeconds(maxCreateConnectionTime))
+                        .connectTimeout(Duration.ofSeconds(60))
                         .build()
         );
 
         ConnectionPoolConfiguration config = ConnectionPoolConfiguration.builder(factory)
                 .initialSize(initialPoolSize)
                 .maxSize(maxPoolSize)
-                .maxIdleTime(Duration.ofMinutes(maxIdleTime))
+                .maxIdleTime(Duration.ofSeconds(30))
                 .maxCreateConnectionTime(Duration.ofSeconds(10))
                 .build();
 
