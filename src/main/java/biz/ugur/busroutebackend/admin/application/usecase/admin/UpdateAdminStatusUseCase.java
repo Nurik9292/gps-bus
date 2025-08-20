f9366f0d24be54e116af424f33162520ec391d55
Timur <nurydavletov@gmail.com>
2025-08-20 13:05:09 +0300

    change usecase update status admin

--- src/main/java/biz/ugur/busroutebackend/admin/application/usecase/admin/UpdateAdminStatusUseCase.java
+++ src/main/java/biz/ugur/busroutebackend/admin/application/usecase/admin/UpdateAdminStatusUseCase.java
@@ -5,7 +5,8 @@ import biz.ugur.busroutebackend.admin.domain.exceptions.AdminNotFoundException;
 import biz.ugur.busroutebackend.admin.domain.repository.AdminRepository;
 import biz.ugur.busroutebackend.admin.domain.valueobjects.AdminId;
 import biz.ugur.busroutebackend.shared.application.CorrelationContextService;
-import biz.ugur.busroutebackend.shared.application.UseCase;
+import biz.ugur.busroutebackend.shared.application.EventBus;
+import biz.ugur.busroutebackend.shared.base.BaseUseCase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -13,23 +14,32 @@ import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
-public class UpdateAdminStatusUseCase  implements UseCase<Mono<UpdateAdminStatusUseCase.Request>, Mono<AdminResult>> {
+public class UpdateAdminStatusUseCase  extends BaseUseCase<Mono<UpdateAdminStatusUseCase.Request>, AdminResult> {
 
     private final AdminRepository adminRepository;
     private final CorrelationContextService correlationService;
 
-    public UpdateAdminStatusUseCase(AdminRepository adminRepository, CorrelationContextService correlationService) {
+    public UpdateAdminStatusUseCase(AdminRepository adminRepository,
+                                    CorrelationContextService correlationService,
+                                    EventBus eventBus) {
+        super(correlationService, eventBus);
         this.adminRepository = adminRepository;
         this.correlationService = correlationService;
     }
 
+
+
     @Override
-    public Mono<AdminResult> execute(Mono<Request> request) {
-       return  correlationService.executeWithCorrelation(
-               request.flatMap(this::executeWithCorrelation), "admin");
+    protected Mono<AdminResult> process(Mono<Request> request) {
+        return request.flatMap(this::processInternal);
     }
 
-    private Mono<AdminResult> executeWithCorrelation(Request request) {
+    @Override
+    protected String getBoundContext() {
+        return "admin";
+    }
+
+    private Mono<AdminResult> processInternal(Request request) {
        return correlationService.getCurrentCorrelationId().flatMap(correlationId -> {
            return adminRepository.findById(AdminId.of(request.id))
                            .switchIfEmpty(Mono.error(new AdminNotFoundException(request.id, "id", correlationId)))
