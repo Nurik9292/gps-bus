6e133478db85a35bbefc7c1ae86b3d35b4de8cc6
Timur <nurydavletov@gmail.com>
2025-08-20 13:47:40 +0300

    change usecase delete city

--- src/main/java/biz/ugur/busroutebackend/admin/application/usecase/city/DeleteCityUseCase.java
+++ src/main/java/biz/ugur/busroutebackend/admin/application/usecase/city/DeleteCityUseCase.java
@@ -3,29 +3,37 @@ package biz.ugur.busroutebackend.admin.application.usecase.city;
 import biz.ugur.busroutebackend.admin.domain.repository.CityRepository;
 import biz.ugur.busroutebackend.admin.domain.valueobjects.CityId;
 import biz.ugur.busroutebackend.shared.application.CorrelationContextService;
-import biz.ugur.busroutebackend.shared.application.UseCase;
+import biz.ugur.busroutebackend.shared.application.EventBus;
+import biz.ugur.busroutebackend.shared.base.BaseUseCase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
-public class DeleteCityUseCase implements UseCase<Mono<String>, Mono<Void>> {
+public class DeleteCityUseCase extends BaseUseCase<Mono<String>, Void> {
 
     private final CityRepository cityRepository;
-    private final CorrelationContextService correlationService;
 
-    public DeleteCityUseCase(CityRepository cityRepository, CorrelationContextService correlationService) {
+    public DeleteCityUseCase(CityRepository cityRepository,
+                             CorrelationContextService correlationService,
+                             EventBus eventBus) {
+        super(correlationService, eventBus);
         this.cityRepository = cityRepository;
-        this.correlationService = correlationService;
+    }
+
+
+    @Override
+    protected Mono<Void> process(Mono<String> request) {
+        return request.flatMap(this::processInternal);
     }
 
     @Override
-    public Mono<Void> execute(Mono<String> idCity) {
-        return correlationService.executeWithCorrelation(idCity.flatMap(this::executeWithCorrelation), "admin");
+    protected String getBoundContext() {
+        return "admin";
     }
 
-    private Mono<Void> executeWithCorrelation(String cityId) {
+    private Mono<Void> processInternal(String cityId) {
         return correlationService.getCurrentCorrelationId().flatMap(correlationId -> {
             log.info("Deleting city  Correlation {}  ID: {}", correlationId,  cityId);
 
