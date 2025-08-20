b6b0967cbad9c57dbdbf7ea7a7a93658d5453b1a
Timur <nurydavletov@gmail.com>
2025-08-20 12:56:01 +0300

    change usecase deleteAdmin

--- src/main/java/biz/ugur/busroutebackend/admin/application/usecase/admin/DeleteAdminUseCase.java
+++ src/main/java/biz/ugur/busroutebackend/admin/application/usecase/admin/DeleteAdminUseCase.java
@@ -5,30 +5,37 @@ import biz.ugur.busroutebackend.admin.domain.exceptions.AdminNotFoundException;
 import biz.ugur.busroutebackend.admin.domain.repository.AdminRepository;
 import biz.ugur.busroutebackend.admin.domain.valueobjects.AdminId;
 import biz.ugur.busroutebackend.shared.application.CorrelationContextService;
-import biz.ugur.busroutebackend.shared.application.UseCase;
+import biz.ugur.busroutebackend.shared.application.EventBus;
+import biz.ugur.busroutebackend.shared.base.BaseUseCase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
-public class DeleteAdminUseCase implements UseCase<Mono<DeleteAdminUseCase.Request>, Mono<Void>> {
+public class DeleteAdminUseCase extends BaseUseCase<Mono<DeleteAdminUseCase.Request>, Void> {
 
     private final AdminRepository adminRepository;
-    private final CorrelationContextService correlationService;
 
-    public DeleteAdminUseCase(AdminRepository adminRepository, CorrelationContextService correlationService) {
+    public DeleteAdminUseCase(AdminRepository adminRepository,
+                              CorrelationContextService correlationService,
+                              EventBus eventBus) {
+        super(correlationService, eventBus);
         this.adminRepository = adminRepository;
-        this.correlationService = correlationService;
+    }
+
+
+    @Override
+    protected Mono<Void> process(Mono<Request> request) {
+        return request.flatMap(this::processInternal);
     }
 
     @Override
-    public Mono<Void> execute(Mono<Request> request) {
-        return correlationService.executeWithCorrelation(
-                request.flatMap(this::executeWithCorrelation), "admin");
+    protected String getBoundContext() {
+        return "admin";
     }
 
-    private Mono<Void> executeWithCorrelation(Request request) {
+    private Mono<Void> processInternal(Request request) {
         return correlationService.getCurrentCorrelationId()
                 .flatMap(correlationId -> {
                     String idValue = request.adminId;
