6cd05e9f128232761747b436967d0fe69e1d6ec8
Timur <nurydavletov@gmail.com>
2025-08-20 13:50:27 +0300

    change usecase get city by id

--- src/main/java/biz/ugur/busroutebackend/admin/application/usecase/city/GetCityByIdUseCase.java
+++ src/main/java/biz/ugur/busroutebackend/admin/application/usecase/city/GetCityByIdUseCase.java
@@ -4,32 +4,40 @@ import biz.ugur.busroutebackend.admin.application.dto.city.CityResult;
 import biz.ugur.busroutebackend.admin.domain.repository.CityRepository;
 import biz.ugur.busroutebackend.admin.domain.valueobjects.CityId;
 import biz.ugur.busroutebackend.shared.application.CorrelationContextService;
-import biz.ugur.busroutebackend.shared.application.UseCase;
+import biz.ugur.busroutebackend.shared.application.EventBus;
+import biz.ugur.busroutebackend.shared.base.BaseUseCase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
-public class GetCityByIdUseCase implements UseCase<Mono<String>, Mono<CityResult>> {
+public class GetCityByIdUseCase extends BaseUseCase<Mono<String>, CityResult> {
 
     private final CityRepository cityRepository;
-    private final CorrelationContextService correlationService;
 
-    public GetCityByIdUseCase(CityRepository cityRepository, CorrelationContextService correlationService) {
+    public GetCityByIdUseCase(CityRepository cityRepository,
+                              CorrelationContextService correlationService,
+                              EventBus eventBus) {
+        super(correlationService, eventBus);
         this.cityRepository = cityRepository;
-        this.correlationService = correlationService;
+    }
+
+
+    @Override
+    protected Mono<CityResult> process(Mono<String> request) {
+        return request.flatMap(this::processInternal);
     }
 
     @Override
-    public Mono<CityResult> execute(Mono<String> id) {
-      return correlationService.executeWithCorrelation(id.flatMap(this::executeWithCorrelation), "admin");
+    protected String getBoundContext() {
+        return "admin";
     }
 
 
-    private Mono<CityResult> executeWithCorrelation(String id) {
+    private Mono<CityResult> processInternal(String id) {
         return correlationService.getCurrentCorrelationId().flatMap(correlationId -> {
-            log.debug("Getting city by CorrelationId - {} ID: {}", correlationId,  id);
+            log.debug("Getting city by CorrelationId - {} CityID: {}", correlationId,  id);
 
             return cityRepository.findById(CityId.of(id))
                     .switchIfEmpty(Mono.error(new IllegalArgumentException("City not found with ID: " + id)))
