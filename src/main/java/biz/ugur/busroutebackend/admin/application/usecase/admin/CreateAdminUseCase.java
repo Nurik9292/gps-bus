a3db05b25687eaafda89bb258bdf9f485e2c7481
Timur <nurydavletov@gmail.com>
2025-08-20 14:55:12 +0300

    change usecase createAdmin

--- src/main/java/biz/ugur/busroutebackend/admin/application/usecase/admin/CreateAdminUseCase.java
+++ src/main/java/biz/ugur/busroutebackend/admin/application/usecase/admin/CreateAdminUseCase.java
@@ -1,39 +1,42 @@
 package biz.ugur.busroutebackend.admin.application.usecase.admin;
 
-import biz.ugur.busroutebackend.admin.application.dto.admin.CreateCommand;
 import biz.ugur.busroutebackend.admin.application.dto.admin.AdminResult;
+import biz.ugur.busroutebackend.admin.application.dto.admin.CreateCommand;
 import biz.ugur.busroutebackend.admin.domain.exceptions.AdminAlreadyExistsException;
 import biz.ugur.busroutebackend.admin.domain.repository.AdminRepository;
 import biz.ugur.busroutebackend.shared.application.CorrelationContextService;
 import biz.ugur.busroutebackend.shared.application.EventBus;
-import biz.ugur.busroutebackend.shared.application.UseCase;
+import biz.ugur.busroutebackend.shared.base.BaseUseCase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
-public class CreateAdminUseCase implements UseCase<Mono<CreateCommand>, Mono<AdminResult>> {
+public class CreateAdminUseCase extends BaseUseCase<Mono<CreateCommand>, AdminResult> {
 
     private final AdminRepository adminRepository;
-    private final EventBus eventBus;
-    private final CorrelationContextService correlationService;
 
     public CreateAdminUseCase(AdminRepository adminRepository,
                               EventBus eventBus,
                               CorrelationContextService correlationService) {
+        super(correlationService, eventBus);
         this.adminRepository = adminRepository;
-        this.eventBus = eventBus;
-        this.correlationService = correlationService;
+    }
+
+
+    @Override
+    protected Mono<AdminResult> process(Mono<CreateCommand> request) {
+        return request.flatMap(this::processInternal);
     }
 
     @Override
-    public Mono<AdminResult> execute(Mono<CreateCommand> commandMono) {
-        return correlationService.executeWithCorrelation(
-                commandMono.flatMap(this::executeWithCorrelation), "admin");
+    protected String getBoundContext() {
+        return "admin";
     }
 
-    private Mono<AdminResult> executeWithCorrelation(CreateCommand command) {
+
+    private Mono<AdminResult> processInternal(CreateCommand command) {
         return correlationService.getCurrentCorrelationId()
                 .flatMap(correlationId -> {
                     log.info("Creating admin - CorrelationId: {} - Username: {}",
@@ -46,12 +49,7 @@ public class CreateAdminUseCase implements UseCase<Mono<CreateCommand>, Mono<Adm
                                             correlationId.value(), command.username());
                                     return Mono.error(new AdminAlreadyExistsException(command.username(), correlationId));
                                 }
-
                                 return adminRepository.save(command.toDomain())
-                                        .doOnNext(savedAdmin -> {
-                                            savedAdmin.getUncommittedEvents().forEach(eventBus::publish);
-                                            savedAdmin.markEventsAsCommitted();
-                                        })
                                         .map(AdminResult::fromDomain)
                                         .doOnSuccess(result ->
                                                 log.info("Admin created successfully - CorrelationId: {} - Username: {}",
