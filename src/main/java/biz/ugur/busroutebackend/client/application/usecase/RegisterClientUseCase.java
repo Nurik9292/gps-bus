bf4e4c919ea904f6d9915b48db6f19e770c8324d
Timur <nurydavletov@gmail.com>
2025-08-21 08:24:37 +0300

    change usecase register client

--- src/main/java/biz/ugur/busroutebackend/client/application/usecase/RegisterClientUseCase.java
+++ src/main/java/biz/ugur/busroutebackend/client/application/usecase/RegisterClientUseCase.java
@@ -3,37 +3,62 @@ package biz.ugur.busroutebackend.client.application.usecase;
 import biz.ugur.busroutebackend.client.domain.enums.Platform;
 import biz.ugur.busroutebackend.client.domain.model.Client;
 import biz.ugur.busroutebackend.client.domain.repository.ClientRepository;
+import biz.ugur.busroutebackend.shared.application.CorrelationContextService;
+import biz.ugur.busroutebackend.shared.application.EventBus;
 import biz.ugur.busroutebackend.shared.application.UseCase;
+import biz.ugur.busroutebackend.shared.base.BaseUseCase;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Log4j2
 @Service
-@RequiredArgsConstructor
-public class RegisterClientUseCase implements UseCase<RegisterClientUseCase.Command, Mono<RegisterClientUseCase.Result>> {
+public class RegisterClientUseCase extends BaseUseCase<Mono<RegisterClientUseCase.Command>, RegisterClientUseCase.Result> {
 
     private final ClientRepository clientRepository;
 
+    public RegisterClientUseCase(ClientRepository clientRepository,
+                                 CorrelationContextService correlationContextService,
+                                 EventBus eventBus) {
+        super(correlationContextService, eventBus);
+        this.clientRepository = clientRepository;
+    }
+
+
     @Override
-    public Mono<Result> execute(Command command) {
-        return clientRepository.existsByPhone(command.phone())
-                .flatMap(exists -> {
-                    if (exists) {
-                        return Mono.error(new IllegalArgumentException("Phone number already registered"));
-                    }
+    protected Mono<Result> process(Mono<Command> command) {
+        return command.flatMap(this::processInternal);
+    }
 
-                    Client client = new Client(command.name(), command.phone(), command.platform());
-                    client.generateOtp();
+    @Override
+    protected String getBoundContext() {
+        return "client";
+    }
 
-                    return clientRepository.save(client)
-                            .map(savedClient -> new Result(
-                                    savedClient.getId().getValue(),
-                                    savedClient.getName(),
-                                    savedClient.getPhoneNumber(),
-                                    savedClient.getOtpCode(),
-                                    savedClient.getStatus().name()
-                            ));
-                });
+    private Mono<Result> processInternal(Command command) {
+        return correlationService.getCurrentCorrelationId().flatMap(correlationId -> {
+            log.info("Register client CorrelationId: {} - Client Phone: {}", correlationId, command.phone());
+
+            return clientRepository.existsByPhone(command.phone())
+                    .flatMap(exists -> {
+                        if (exists) {
+                            return Mono.error(new IllegalArgumentException("Phone number already registered"));
+                        }
+
+                        Client client = new Client(command.name(), command.phone(), command.platform());
+                        client.generateOtp();
+
+                        return clientRepository.save(client)
+                                .map(savedClient -> new Result(
+                                        savedClient.getId().getValue(),
+                                        savedClient.getName(),
+                                        savedClient.getPhoneNumber(),
+                                        savedClient.getOtpCode(),
+                                        savedClient.getStatus().name()
+                                ));
+                    });
+        });
     }
 
     public record Command(String name, String phone, Platform platform) {}
