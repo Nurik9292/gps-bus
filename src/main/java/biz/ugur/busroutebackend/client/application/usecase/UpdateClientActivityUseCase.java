632b59c8e51b9d804bc8bdc31f270b817c9b151c
Timur <nurydavletov@gmail.com>
2025-08-21 08:26:59 +0300

    change usecase update status client

--- src/main/java/biz/ugur/busroutebackend/client/application/usecase/UpdateClientActivityUseCase.java
+++ src/main/java/biz/ugur/busroutebackend/client/application/usecase/UpdateClientActivityUseCase.java
@@ -1,27 +1,49 @@
 package biz.ugur.busroutebackend.client.application.usecase;
 
-import biz.ugur.busroutebackend.client.domain.valueobject.ClientId;
 import biz.ugur.busroutebackend.client.domain.repository.ClientRepository;
-import biz.ugur.busroutebackend.shared.application.UseCase;
-import lombok.RequiredArgsConstructor;
+import biz.ugur.busroutebackend.client.domain.valueobject.ClientId;
+import biz.ugur.busroutebackend.shared.application.CorrelationContextService;
+import biz.ugur.busroutebackend.shared.application.EventBus;
+import biz.ugur.busroutebackend.shared.base.BaseUseCase;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Log4j2
 @Service
-@RequiredArgsConstructor
-public class UpdateClientActivityUseCase implements UseCase<UpdateClientActivityUseCase.Command, Mono<Void>> {
+public class UpdateClientActivityUseCase extends BaseUseCase<Mono<UpdateClientActivityUseCase.Command>, Void> {
 
     private final ClientRepository clientRepository;
 
+    public UpdateClientActivityUseCase(ClientRepository clientRepository,
+                                       CorrelationContextService contextService,
+                                       EventBus eventBus) {
+        super(contextService, eventBus);
+        this.clientRepository = clientRepository;
+    }
+
     @Override
-    public Mono<Void> execute(Command command) {
-        return clientRepository.findById(ClientId.of(command.clientId()))
-                .switchIfEmpty(Mono.error(new IllegalArgumentException("Client not found")))
-                .flatMap(client -> {
-                    client.updateActivity();
-                    return clientRepository.save(client);
-                })
-                .then();
+    protected Mono<Void> process(Mono<Command> command) {
+        return command.flatMap(this::processInternal);
+    }
+
+    @Override
+    protected String getBoundContext() {
+        return "client";
+    }
+
+    private Mono<Void> processInternal(Command command) {
+        return correlationService.getCurrentCorrelationId().flatMap(correlationId -> {
+            log.info("Update status client CorrelationId: {} - ClientId: {} ", correlationId, command.clientId());
+
+            return clientRepository.findById(ClientId.of(command.clientId()))
+                    .switchIfEmpty(Mono.error(new IllegalArgumentException("Client not found")))
+                    .flatMap(client -> {
+                        client.updateActivity();
+                        return clientRepository.save(client);
+                    })
+                    .then();
+        });
     }
 
     public record Command(String clientId) {}
