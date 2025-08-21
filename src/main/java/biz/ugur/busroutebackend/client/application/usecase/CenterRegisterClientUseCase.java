f77222800b42e5881152b0be9630385232907b55
Timur <nurydavletov@gmail.com>
2025-08-21 08:21:52 +0300

    change usecase center register client

--- src/main/java/biz/ugur/busroutebackend/client/application/usecase/CenterRegisterClientUseCase.java
+++ src/main/java/biz/ugur/busroutebackend/client/application/usecase/CenterRegisterClientUseCase.java
@@ -5,40 +5,44 @@ import biz.ugur.busroutebackend.client.domain.model.Client;
 import biz.ugur.busroutebackend.client.domain.repository.ClientRepository;
 import biz.ugur.busroutebackend.client.infrastructure.security.JwtTokenService;
 import biz.ugur.busroutebackend.shared.application.CorrelationContextService;
-import biz.ugur.busroutebackend.shared.application.UseCase;
+import biz.ugur.busroutebackend.shared.application.EventBus;
+import biz.ugur.busroutebackend.shared.base.BaseUseCase;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 @Log4j2
-public class CenterRegisterClientUseCase implements
-        UseCase<CenterRegisterClientUseCase.Command, Mono<CenterRegisterClientUseCase.Result>> {
+public class CenterRegisterClientUseCase extends
+        BaseUseCase<Mono<CenterRegisterClientUseCase.Command>, CenterRegisterClientUseCase.Result> {
 
     private final ClientRepository clientRepository;
-    private final CorrelationContextService correlationContextService;
     private final JwtTokenService jwtTokenService;
 
     public CenterRegisterClientUseCase(ClientRepository clientRepository,
+                                       JwtTokenService jwtTokenService,
                                        CorrelationContextService correlationContextService,
-                                       JwtTokenService jwtTokenService) {
+                                       EventBus eventBus) {
+        super(correlationContextService, eventBus);
         this.clientRepository = clientRepository;
-        this.correlationContextService = correlationContextService;
         this.jwtTokenService = jwtTokenService;
     }
 
     @Override
-    public Mono<Result> execute(Command command) {
-        return correlationContextService.executeWithCorrelation(
-                Mono.just(command).flatMap(this::executeWithCorrelation),
-                "mobile"
-        );
+    protected Mono<Result> process(Mono<Command> command) {
+        return command.flatMap(this::processInternal);
     }
 
-    private Mono<Result> executeWithCorrelation(Command command) {
-        return correlationContextService.getCurrentCorrelationId()
+    @Override
+    protected String getBoundContext() {
+        return "client";
+    }
+
+
+    private Mono<Result> processInternal(Command command) {
+        return correlationService.getCurrentCorrelationId()
                 .doOnNext(correlationId ->
-                        log.info("Center auth client Correlation Id: {} client phone: {}", correlationId, command.phone))
+                        log.info("Center auth client Correlation Id: {} - Client Phone: {}", correlationId, command.phone))
                 .flatMap(id -> findOrCreateClient(command)
                         .flatMap(clientRepository::save)
                         .map(this::buildResult));
@@ -73,6 +77,8 @@ public class CenterRegisterClientUseCase implements
                 savedClient.getStatus().name()
         );
     }
+
+
 
     public record Command(String phone, String platform) {}
 
